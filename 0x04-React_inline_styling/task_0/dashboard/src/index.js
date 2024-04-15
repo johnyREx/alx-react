e8678dc041f9dd1ed4368892ccf15995e4d5666f
johnyREx <johnkwofie99@icloud.com>
2024-04-15 16:45:03 +0300

    is you like that

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-Dom/client";
+import App from "./App/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <App />
+    </React.StrictMode>
+);
