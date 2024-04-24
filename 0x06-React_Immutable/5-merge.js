3b84f90c052905c06d97ae7fb5d125fab8d5a7ec
johnyREx <johnkwofie99@icloud.com>
2024-04-24 16:04:31 +0200

    the clouds aint real

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,6 +1,5 @@
- import { List, Map } from 'immutable';
+import { List, Map } from 'immutable';
 
- export const contactElements = (page, page2) => List(page1).contact(List(page2));
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
 
- export const mergeElements = (page1, page2) => Map(page1).contact(Map(page2));
- 
+export const mergeElements = (page1, page2) => Map(page1).concat(Map(page2));
