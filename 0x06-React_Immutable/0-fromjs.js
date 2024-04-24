d6ac359b2350d8bf9e4ff538883c1d6b155e991f
johnyREx <johnkwofie99@icloud.com>
2024-04-24 15:30:40 +0200

    hunting wabbitz

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,6 +1,6 @@
 // import { fromJS } from 'immutable';
-import { fromJS } from '.node_modules/immutable/dist/immutable';
+import { fromJS } from './node_modules/immutable/dist/immutable';
 
-const getImmutableObject = (Object) => fromJS(Object);
+const getImmutableObject = (object) => fromJS(object);
 
 export default getImmutableObject;
